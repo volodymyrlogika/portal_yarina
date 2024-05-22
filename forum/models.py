93773072ef27652064763b4212e0f65ffbce4a62
Yaryna Boiko <yaryna.boiko06@gmail.com>
2024-05-22 18:31:53 +0300

    Додала модель для форуму

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Topic(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Тема')
+    description = models.TextField(verbose_name='Опис')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ads', verbose_name='Автор')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Створено')
+
+
+class Message(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='message', verbose_name='Тема')
+    text = models.TextField(verbose_name='Опис')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ads', verbose_name='Автор')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Створено')
