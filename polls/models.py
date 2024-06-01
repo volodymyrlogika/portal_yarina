865e9d7b32cf90e324c0503095767627f7e1efe2
Yaryna Boiko <yaryna.boiko06@gmail.com>
2024-06-01 22:23:11 +0300

    Додала модель для форуму

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Polls(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Назва опитування")
+    description = models.TextField(verbose_name="Опис опитування")
+    question = models.TextField(verbose_name="Питання")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='polls', verbose_name='Автор')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Створено')
+    image = models.ImageField(verbose_name='Картинка', upload_to='polls_images/', blank=True, null=True)
+    choice = models.CharField(max_length=30)
