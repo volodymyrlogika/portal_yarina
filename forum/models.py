dc6bd78194c8ef320297afe9a11148acd3c81d2d
Yaryna Boiko <yaryna.boiko06@gmail.com>
2024-06-02 14:40:06 +0300

    Додала моделі для polls, portfolio та ads

--- forum/models.py
+++ forum/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Topic(models.Model):
     title = models.CharField(max_length=200, verbose_name='Тема')
     description = models.TextField(verbose_name='Опис')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ads', verbose_name='Автор')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='topics', verbose_name='Автор')
     created = models.DateTimeField(auto_now_add=True, verbose_name='Створено')
 
     def __str__(self):
@@ -14,7 +14,7 @@ class Topic(models.Model):
 class Message(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='message', verbose_name='Тема')
     text = models.TextField(verbose_name='Опис')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ads', verbose_name='Автор')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages', verbose_name='Автор')
     created = models.DateTimeField(auto_now_add=True, verbose_name='Створено')
 
     def __str__(self):
